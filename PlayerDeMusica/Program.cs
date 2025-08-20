4daf20015765cebf6ac24b4b50c0874a08790fb7
Gabriel Caetano <129672970+gabrielctnn@users.noreply.github.com>
2025-08-19 21:49:29 -0300

    correção de texto da questao no codigo

--- PlayerDeMusica/Program.cs
+++ PlayerDeMusica/Program.cs
@@ -11,7 +11,7 @@ namespace PlayerDeMusica
     {
         static void Main(string[] args)
         {
-            // 7 - Crie uma classe chamada Música que contenha os atributos nome, autor e gravadora.
+            // 6 - Crie uma classe chamada Música que contenha os atributos nome, autor e gravadora.
             // Após, crie uma classe Playlist que possua como atributo uma Lista de músicas e uma string para armazenar o dono da playlist.
             // Implemente um método para adicionar músicas na lista, para "tocar a música" (só mostrar uma mensagem na tela com o titulo da música).
             // Tente fazer uma reprodução aleatória, ou seja, em vez de percorrer do inicio ao fim da lista, faça um random para acessar valores aleatórios válidos.
